071edfa7fcc04f54a76d5e444d2db7df08f94df6
kinimesi <ilkinsafarli@std.sehir.edu.tr>
2017-03-30 11:49:00 +0300

    Get expand-collapse from unstable branch

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var port = process.env.port || 80;
 
 server.listen(port, function(){
-  console.log('server listening on port%d', port);
+  console.log('server listening on port%d: ', port);
 });
 
 app.use(express.static(__dirname));
