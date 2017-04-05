1018765251636c4cfdcc5962a6cd6b8a112df6c3
fdurupinar <fdurupinar@users.noreply.github.com>
2017-04-05 14:23:39 -0700

    Make appUtilities global

--- app/main.js
+++ app/main.js
@@ -7,7 +7,7 @@ var cytoscape = require('cytoscape');
 require('jquery-expander')($);
 require('bootstrap');
 
-var appUtilities = require('./js/app-utilities');
+appUtilities = require('./js/app-utilities');
 var appUndoActions = require('./js/app-undo-actions');
 var appCy = require('./js/app-cy');
 var appMenu = require('./js/app-menu');
