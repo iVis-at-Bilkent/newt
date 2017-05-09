14b0fb1f1275e76a31b7d24ada022bfb271f3dc8
Ludovic Roy <lroy@eisbm.org>
2017-05-09 11:49:04 +0200

    add pointer cursor to some inspector buttons

--- app/js/inspector-utilities.js
+++ app/js/inspector-utilities.js
@@ -17,7 +17,7 @@ inspectorUtilities.fillInspectorStateAndInfos = function (nodes, stateAndInfos, 
                 + "<span width='" + width / 5 + "'px>@</span>"
                 + "<input type='text' id='inspector-state-variable-variable" + i + "' class='inspector-input-box' style='width: "
                 + width / 2.5 + "px;' value='" + (state.state.variable || '')
-                + "'/><img width='12px' height='12px' id='inspector-delete-state-and-info" + i + "' class='inspector-input-box' src='app/img/delete.png'></img></div>");
+                + "'/><img width='12px' height='12px' id='inspector-delete-state-and-info" + i + "' class='inspector-input-box pointer-button' src='app/img/delete.png'></img></div>");
 
         $("#inspector-state-variable-value" + i).unbind('change').on('change', function () {
           chise.changeStateOrInfoBox(nodes, i, $(this).val(), 'value');
@@ -31,7 +31,7 @@ inspectorUtilities.fillInspectorStateAndInfos = function (nodes, stateAndInfos, 
         var total = width / 1.25;
         $("#inspector-unit-of-informations").append("<div><input type='text' id='inspector-unit-of-information-label" + i + "' class='inspector-input-box' style='width: "
                 + total + "px;' value='" + (state.label.text || '')
-                + "'/><img width='12px' height='12px' id='inspector-delete-state-and-info" + i + "' class='inspector-input-box' src='app/img/delete.png'></img></div>");
+                + "'/><img width='12px' height='12px' id='inspector-delete-state-and-info" + i + "' class='inspector-input-box pointer-button' src='app/img/delete.png'></img></div>");
 
         $("#inspector-unit-of-information-label" + i).unbind('change').on('change', function () {
           chise.changeStateOrInfoBox(nodes, i, $(this).val());
@@ -44,8 +44,8 @@ inspectorUtilities.fillInspectorStateAndInfos = function (nodes, stateAndInfos, 
       });
     })(i);
   }
-  $("#inspector-state-variables").append("<img id='inspector-add-state-variable' src='app/img/add.png'/>");
-  $("#inspector-unit-of-informations").append("<img id='inspector-add-unit-of-information' src='app/img/add.png'/>");
+  $("#inspector-state-variables").append("<img id='inspector-add-state-variable' src='app/img/add.png' class='pointer-button'/>");
+  $("#inspector-unit-of-informations").append("<img id='inspector-add-unit-of-information' src='app/img/add.png' class='pointer-button'/>");
 
   $("#inspector-add-state-variable").click(function () {
     var obj = {};
@@ -205,7 +205,7 @@ inspectorUtilities.handleSBGNInspector = function () {
             title = "lock aspect ratio";
           }
           
-          html += "<img id='inspector-node-sizes-aspect-ratio' style='vertical-align: top; margin-left: 5px;' src='app/img/";
+          html += "<img id='inspector-node-sizes-aspect-ratio' style='vertical-align: top; margin-left: 5px;' class='pointer-button' src='app/img/";
           html += imageName;
           html += "'";
           
