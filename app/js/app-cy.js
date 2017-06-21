c39eb3094a2d35073df18dbc624a2c2a6b1557a2
kinimesi <kinimesi@users.noreply.github.com>
2017-06-21 16:02:15 +0300

    Add edges from panel with parameters

--- app/js/app-cy.js
+++ app/js/app-cy.js
@@ -299,9 +299,8 @@ module.exports = function () {
           // fired when edgehandles is done and entities are added
           var source = sourceNode.id();
           var target = targetNodes[0].id();
-          var edgeclass = modeHandler.selectedEdgeType;
-
-          chise.addEdge(source, target, edgeclass);
+          var edgeParams = {class : modeHandler.selectedEdgeType, language : modeHandler.selectedLanguage};
+          chise.addEdge(source, target, edgeParams);
           
           // If not in sustain mode set selection mode
           if (!modeHandler.sustainMode) {
