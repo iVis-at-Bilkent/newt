iVis-at-Bilkent/newt

commit 8dfd6dc92272d4e6cb2452544a4f9a82be283e63

Resolves #78

app/js/app-mode-handler.js
@@ -53,6 +53,9 @@ var modeHandler = {
       cy.autoungrabify(true);
       cy.autounselectify(true);
     }
+    else if(!modeHandler.sustainMode){
+        modeHandler.setSelectionMode();
+    }
 
     // Check if there is a needed type change if there is perform it.
     if ( typeChange ) {
@@ -91,6 +94,9 @@ var modeHandler = {
       cy.autounselectify(true);
 
       cy.edgehandles('drawon');
+    }
+    else if(!modeHandler.sustainMode){
+        modeHandler.setSelectionMode();
     }
 
     // Check if there is a needed type change if there is perform it.

commit 3a663a3a0b9949f12394c342870902310f9f3f1a

Resolves a bug after 8dfd6dc92272d4e6cb2452544a4f9a82be283e63 introduced for #78

app/js/app-mode-handler.js
@@ -53,7 +53,7 @@ var modeHandler = {
       cy.autoungrabify(true);
       cy.autounselectify(true);
     }
-    else if(!modeHandler.sustainMode){
+    else if(!modeHandler.sustainMode && !typeChange){
         modeHandler.setSelectionMode();
     }
 
@@ -97,7 +97,7 @@ var modeHandler = {
 
       cy.edgehandles('drawon');
     }
-    else if(!modeHandler.sustainMode){
+    else if(!modeHandler.sustainMode && !typeChange){
         modeHandler.setSelectionMode();
     }
 