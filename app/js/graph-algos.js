f1f6bc81b4391e130c03817cd185cdeb54499927
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-07 11:20:45 +0300

    Added pathsFromTo

--- app/js/graph-algos.js
+++ app/js/graph-algos.js
@@ -1,6 +1,13 @@
 var jquery = ($ = require("jquery"));
 
 var graphAlgos = {
-    
+    pathsFromTo: function(sourceArray, targetArray, limit)
+    {
+        return `UNWIND $sourceArray as source WITH source 
+        UNWIND $targetArray as target 
+        WITH source, target 
+        match p=(a {entityName: source})-[*..${limit}]-(b {entityName: target}) 
+        return a, b, p`
+    }
 }
 module.exports = graphAlgos;
