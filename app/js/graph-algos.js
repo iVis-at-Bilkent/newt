bceb4395df3666f90bf746ca194cb751c0d8c5f0
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-15 11:17:40 +0300

    fix bug in graph algo

--- app/js/graph-algos.js
+++ app/js/graph-algos.js
@@ -28,21 +28,21 @@ var graphAlgos = {
     commonStream: function(idList, lengthLimit)
     {
         var pageSize = 100000;
-        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 1,
+        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
             return query
     },
     upstream: function(idList, lenghtLimit)
     {
         var pageSize = 100000;
-        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 1,
+        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
             return query
     },
     downstream: function(idList, lengthLimit)
     {
         var pageSize = 100000;
-        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 0,
+        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 0,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
             return query
     }
