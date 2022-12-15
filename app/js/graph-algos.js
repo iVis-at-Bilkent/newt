effcc33305592ec6e9fafa62b8eda5fb247d8ada
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-15 10:57:46 +0300

    Added all the cypher queries for graph query

--- app/js/graph-algos.js
+++ app/js/graph-algos.js
@@ -17,6 +17,34 @@ var graphAlgos = {
         var query = `CALL graphOfInterest([${idList}], [], ${lengthLimit}, false,
         ${pageSize}, 1, '', true, '', 0, {}, 0, 1000, 0, 1000000, [])`;
         return query
+    },
+    neighborhood: function(idList, lengthLimit)
+    {
+        var pageSize = 100000;
+        var query = `CALL neighborhood([${idList}], [], ${lengthLimit}, false,
+            ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
+            return query
+    },
+    commonStream: function(idList, lengthLimit)
+    {
+        var pageSize = 100000;
+        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 1,
+            ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
+            return query
+    },
+    upstream: function(idList, lenghtLimit)
+    {
+        var pageSize = 100000;
+        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 1,
+            ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
+            return query
+    },
+    downstream: function(idList, lengthLimit)
+    {
+        var pageSize = 100000;
+        var query = `CALL commonStream([${idList}],, [], ${lengthLimit}, 0,
+            ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
+            return query
     }
 }
 module.exports = graphAlgos;
