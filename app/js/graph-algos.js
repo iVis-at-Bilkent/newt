6f75513a88dd0898f58f951dd32b5ff79b2dbfe4
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2022-12-22 00:52:46 +0300

    Styling and cypher added for neighbor algo

--- app/js/graph-algos.js
+++ app/js/graph-algos.js
@@ -1,50 +1,47 @@
 var jquery = ($ = require("jquery"));
 
 var graphAlgos = {
-    pathsFromTo: function(sourceArray, targetArray, limit)
-    {
-        return `UNWIND $sourceArray as source WITH source 
+  pathsFromTo: function (sourceArray, targetArray, limit) {
+    return `UNWIND $sourceArray as source WITH source 
         UNWIND $targetArray as target 
         WITH source, target 
         match p=(a {entityName: source})-[*..${limit}]-(b {entityName: target}) 
-        return a, b, p`
-    },
+        return a, b, p`;
+  },
 
-    pathsBetween: function(idList, lengthLimit)
-    {
-        console.log("idList",idList)
-        var pageSize = 100000;
-        var query = `CALL graphOfInterest([${idList}], [], ${lengthLimit}, false,
+  pathsBetween: function (idList, lengthLimit) {
+    console.log("idList", idList);
+    var pageSize = 100000;
+    var query = `CALL graphOfInterest([${idList}], [], ${lengthLimit}, false,
         ${pageSize}, 1, '', true, '', 0, {}, 0, 1000, 0, 1000000, [])`;
-        return query
-    },
-    neighborhood: function(idList, lengthLimit)
-    {
-        var pageSize = 100000;
-        var query = `CALL neighborhood([${idList}], ['belongs-to-compartment'], ${lengthLimit}, false,
+    return query;
+  },
+  neighborhood: function (idList, lengthLimit) {
+    var query = `UNWIND $idList as ids
+        MATCH p=(a)-[rels*..${lengthLimit}]-(b)
+        WHERE id(a) = ids and  NONE (r IN rels WHERE type(r)= 'belongs_to_compartment')  
+        and  NONE (r IN rels WHERE type(r)= 'belongs_to_submap')
+        and  NONE (r IN rels WHERE type(r)= 'belongs_to_complex')
+        RETURN nodes(p), relationships(p)`;
+    return query;
+  },
+  commonStream: function (idList, lengthLimit) {
+    var pageSize = 100000;
+    var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
-            return query
-    },
-    commonStream: function(idList, lengthLimit)
-    {
-        var pageSize = 100000;
-        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
+    return query;
+  },
+  upstream: function (idList, lengthLimit) {
+    var pageSize = 100000;
+    var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
-            return query
-    },
-    upstream: function(idList, lengthLimit)
-    {
-        var pageSize = 100000;
-        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 1,
+    return query;
+  },
+  downstream: function (idList, lengthLimit) {
+    var pageSize = 100000;
+    var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 0,
             ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
-            return query
-    },
-    downstream: function(idList, lengthLimit)
-    {
-        var pageSize = 100000;
-        var query = `CALL commonStream([${idList}], [], ${lengthLimit}, 0,
-            ${pageSize}, 1, '', true, '', 0,{}, 0, 0, 0, 100000, [])`;
-            return query
-    }
-}
+    return query;
+  },
+};
 module.exports = graphAlgos;
