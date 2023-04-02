05b018c4d2c64f1aae526a998d61562e2e77be2b
Selbi Ereshova <selbi.ereshova@ug.bilkent.edu.tr>
2023-04-02 13:26:41 +0300

    SBML samples added

--- app/js/app-menu.js
+++ app/js/app-menu.js
@@ -884,7 +884,12 @@ module.exports = function() {
       "#load-sample17" : 'RTN4-controllers-and-binding-proteins.nwt',
       "#load-sample18" : 'signaling-downstream-of-AKT2-3.nwt',
       "#load-sample19" : 'pd_learners_card.nwt',
-      "#load-sample20" : 'af_learners_card.nwt'
+      "#load-sample20" : 'af_learners_card.nwt',
+      "#load-sample21" : 'MODEL1204280021.nwt',
+      "#load-sample22" : 'MODEL2003190002.nwt',
+      "#load-sample23" : 'MODEL2105210001.nwt',
+      "#load-sample24" : 'MODEL1912090002.nwt',
+
     };
 
     for ( var selector in selectorToSampleFileName ) {
