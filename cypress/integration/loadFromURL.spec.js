3a19ac1d248a0db0248b01ac48b313d26a6f2852
Sinan Havus <sinanhavus@gmail.com>
2026-04-07 11:41:23 +0300

    Add additional automated tests #909

--- cypress/integration/loadFromURL.spec.js
+++ cypress/integration/loadFromURL.spec.js
@@ -11,7 +11,16 @@ context('Load from URI/URL', () => {
     });
   });
 
-  it('TC2: URL=https://raw.githubusercontent.com/iVis-at-Bilkent/newt/unstable/app/samples/polyq_proteins_interference.nwt', () => {
+  it('TC2: URI=http://bioregistry.io/reactome:R-HSA-70171', () => {
+    cy.visit(URL + '?URI=http://bioregistry.io/reactome:R-HSA-70171');
+    cy.wait(2000);
+    cy.window().then((win) => {
+      expect(win.cy.nodes().length > 0).to.eq(true);
+      expect(win.cy.edges().length > 0).to.eq(true);
+    });
+  });
+
+  it('TC3: URL=https://raw.githubusercontent.com/iVis-at-Bilkent/newt/unstable/app/samples/polyq_proteins_interference.nwt', () => {
     cy.visit(URL + '?URL=https://raw.githubusercontent.com/iVis-at-Bilkent/newt/unstable/app/samples/polyq_proteins_interference.nwt');
     cy.wait(2000);
     cy.window().then((win) => {
@@ -20,4 +29,30 @@ context('Load from URI/URL', () => {
     });
   });
 
+  it('TC4: URL=https://reactome.org/ContentService/exporter/event/R-HSA-71403.sbgn', () => {
+    cy.visit(URL + '?URL=https://reactome.org/ContentService/exporter/event/R-HSA-71403.sbgn');
+    cy.wait(2000);
+    cy.window().then((win) => {
+      expect(win.cy.nodes().length > 0).to.eq(true);
+      expect(win.cy.edges().length > 0).to.eq(true);
+    });
+  });
+
+  it('TC5: URL=https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000206?filename=BIOMD0000000206_url.xml', () => {
+    cy.visit(URL + '?URL=https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000206?filename=BIOMD0000000206_url.xml');
+    cy.wait(2000);
+    cy.window().then((win) => {
+      expect(win.cy.nodes().length > 0).to.eq(true);
+      expect(win.cy.edges().length > 0).to.eq(true);
+    });
+  });
+
+  it('TC6: URL=https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000001?filename=BIOMD0000000001_url.xml', () => {
+    cy.visit(URL + '?URL=https://www.ebi.ac.uk/biomodels/model/download/BIOMD0000000001?filename=BIOMD0000000001_url.xml');
+    cy.wait(2000);
+    cy.window().then((win) => {
+      expect(win.cy.nodes().length > 0).to.eq(true);
+      expect(win.cy.edges().length > 0).to.eq(true);
+    });
+  });
 });
